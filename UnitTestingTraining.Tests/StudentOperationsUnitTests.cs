5648ab297c191ec32f165d47eca62017e381fe45
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2017-10-08 18:30:19 +0530

    Add unit test for student operations

---
+++ UnitTestingTraining.Tests/StudentOperationsUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestingTraining.BusinessLogics;
+
+namespace UnitTestingTraining.Tests
+{
+    [TestClass]    
+    public class StudentOperationsUnitTests
+    {
+        [TestMethod]
+        public void GetCurrentPercentScoreOfStudent_ReturnsCorrectPercentScoreForDifferentScoresAndMaximumMarks()
+        {
+            var studentOperationsContainer = new StudentOperationsContainer();
+            var percentScored = studentOperationsContainer.GetCurrentPercentScoreOfStudent("100");
+            
+            // I do not know what is the data in the Data store, what do I assert??
+        }
+    }
+}
