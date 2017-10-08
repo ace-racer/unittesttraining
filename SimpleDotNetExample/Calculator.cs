7a840c10d306046693a366ae06ab87b3946afd8b
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2017-10-08 14:33:05 +0530

    Initial code commit

---
+++ SimpleDotNetExample/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SimpleDotNetExample
+{
+    public static class Calculator
+    {        
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int Divide(int a, int b)
+        {
+            if(b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by 0");
+            }
+
+            return a / b;
+        }
+    }
+}
