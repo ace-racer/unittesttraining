fb14fe4fbcc934cabebf769ee48c3a8b2f259812
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2017-10-08 23:12:52 +0530

    Update tests using Fakes and develop 2 unit tests for getting student percent scored

--- UnitTestingTraining.Tests/StudentOperationsUnitTests.cs
+++ UnitTestingTraining.Tests/StudentOperationsUnitTests.cs
@@ -1,23 +1,155 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using DataStoreOperations.Interfaces.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnitTestingTraining.BusinessLogics;
+using UnitTestingTraining.DataStoreOperations.Fakes;
+using UnitTestingTraining.Models;
 
 namespace UnitTestingTraining.Tests
 {
-    [TestClass]    
+    [TestClass]
     public class StudentOperationsUnitTests
     {
         [TestMethod]
         public void GetCurrentPercentScoreOfStudent_ReturnsCorrectPercentScoreForDifferentScoresAndMaximumMarks()
         {
-            var studentOperationsContainer = new StudentOperationsContainer();
-            var percentScored = studentOperationsContainer.GetCurrentPercentScoreOfStudent("100");
-            
-            // I do not know what is the data in the Data store, what do I assert??
+            // Provide your own implementation for the dependencies
+            StubIGeneralOperationsInDataSource generalOperationsInDataStoreContainer = new StubIGeneralOperationsInDataSource();
+            generalOperationsInDataStoreContainer.GetSubjectDetailsBySubjectIdString = (subjectId) =>
+            {
+                switch (subjectId)
+                {
+                    case "11":
+                        return new StudentSubject()
+                        {
+                            Name = "Geography",
+                            Id = "11",
+                            Standard = 5,
+                            TotalMarks = 100
+                        };
+                    case "12":
+                        return new StudentSubject()
+                        {
+                            Name = "Biological Science",
+                            Id = "12",
+                            Standard = 5,
+                            TotalMarks = 50
+                        };                    
+                    default:
+                        return null;
+                }
+            };
+
+            StubIStudentOperationsInDataSource studentOperationsInDataSourceContainer = new StubIStudentOperationsInDataSource();
+            studentOperationsInDataSourceContainer.GetStudentSubjectMappingDetailsForStudentByStudentIdString = (studentId) =>
+            {
+                return new List<StudentSubjectMapping>()
+            {
+                new StudentSubjectMapping()
+                {
+                    StudentId = "11",
+                    SubjectId = "11",
+                    IsActive = true,
+                    CurrentMarksOfStudent = 80,
+                    IsFinalExamConductedForSubject = true
+                },
+                new StudentSubjectMapping()
+                {
+                    StudentId = "11",
+                    SubjectId = "12",
+                    IsActive = true,
+                    CurrentMarksOfStudent = 30,
+                    IsFinalExamConductedForSubject = true
+                }
+            };
+            };
+
+
+            var studentOperationsContainer = new StudentOperationsContainer(generalOperationsInDataStoreContainer, studentOperationsInDataSourceContainer);
+            var actualPercentMarks = studentOperationsContainer.GetCurrentPercentScoreOfStudent("11");
+            double expectedPercentMarks = ((double)110 / 150) * 100;
+            Assert.AreEqual(expectedPercentMarks, actualPercentMarks);            
+        }
+
+        [TestMethod]
+        public void GetCurrentPercentScoreOfStudent_ReturnsCorrectPercentScoreForDifferentScoresMaximumMarksAndFinalExamsForSomeSubjectsAreNotConductedYet()
+        {
+            // Provide your own implementation for the dependencies
+            StubIGeneralOperationsInDataSource generalOperationsInDataStoreContainer = new StubIGeneralOperationsInDataSource();
+            generalOperationsInDataStoreContainer.GetSubjectDetailsBySubjectIdString = (subjectId) =>
+            {
+                switch (subjectId)
+                {
+                    case "11":
+                        return new StudentSubject()
+                        {
+                            Name = "Geography",
+                            Id = "11",
+                            Standard = 5,
+                            TotalMarks = 100
+                        };
+                    case "12":
+                        return new StudentSubject()
+                        {
+                            Name = "Biological Science",
+                            Id = "12",
+                            Standard = 5,
+                            TotalMarks = 50
+                        };
+                    case "13":
+                        return new StudentSubject()
+                        {
+                            Name = "Computer basics",
+                            Id = "13",
+                            Standard = 5,
+                            TotalMarks = 50
+                        };
+                    default:
+                        return null;
+                }
+            };
+
+            StubIStudentOperationsInDataSource studentOperationsInDataSourceContainer = new StubIStudentOperationsInDataSource();
+            studentOperationsInDataSourceContainer.GetStudentSubjectMappingDetailsForStudentByStudentIdString = (studentId) =>
+            {
+                return new List<StudentSubjectMapping>()
+            {
+                new StudentSubjectMapping()
+                {
+                    StudentId = "11",
+                    SubjectId = "11",
+                    IsActive = true,
+                    CurrentMarksOfStudent = 80,
+                    IsFinalExamConductedForSubject = true
+                },
+                new StudentSubjectMapping()
+                {
+                    StudentId = "11",
+                    SubjectId = "12",
+                    IsActive = true,
+                    CurrentMarksOfStudent = 30,
+                    IsFinalExamConductedForSubject = true
+                },
+                 new StudentSubjectMapping()
+                {
+                    StudentId = "11",
+                    SubjectId = "13",
+                    IsActive = true,
+                    CurrentMarksOfStudent = 25,
+                    IsFinalExamConductedForSubject = false
+                },
+            };
+            };
+
+
+            var studentOperationsContainer = new StudentOperationsContainer(generalOperationsInDataStoreContainer, studentOperationsInDataSourceContainer);
+            var actualPercentMarks = studentOperationsContainer.GetCurrentPercentScoreOfStudent("11");
+            double expectedPercentMarks = ((double)110 / 150) * 100;
+            Assert.AreEqual(expectedPercentMarks, actualPercentMarks);
         }
     }
 }
